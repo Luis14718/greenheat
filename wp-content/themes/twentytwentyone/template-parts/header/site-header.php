bc5d5e0ff0123acce08b40d6aa3ec5a6f01eb0d1
luis14718 <luis14718@gmail.com>
2021-08-11 23:26:47 -0600

    logo

--- wp-content/themes/twentytwentyone/template-parts/header/site-header.php
+++ wp-content/themes/twentytwentyone/template-parts/header/site-header.php
@@ -30,7 +30,7 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 			
 		</div>
 		<div class="col-4 text-center">
-		<img src="<?php echo get_bloginfo('url');?>wp-content/uploads/2021/08/Recurso-2.png" alt="">
+		<img src="<?php echo get_bloginfo('url');?>/wp-content/uploads/2021/08/Recurso-2.png" alt="">
 		</div>
 
 		<div class="col-4 text-right">
