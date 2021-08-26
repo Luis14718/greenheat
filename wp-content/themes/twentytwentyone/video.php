dc4a823d7f1e616bec4bd29e105556b3c8600af3
luis14718 <luis14718@gmail.com>
2021-08-26 07:48:55 -0600

    video

--- wp-content/themes/twentytwentyone/video.php
+++ wp-content/themes/twentytwentyone/video.php
@@ -19,11 +19,11 @@ get_header('theme');
         	</div>
         	<div class="row">
         		<div class="col-sm-3 col-lg-2 hidden-xs">
-        			<img class="featured-image" src="<?php echo get_bloginfo('url');?>/sos-websvc/images/default/videos.jpg" alt="Product Knowledge">        		</div>
+        		
         		<div class="col-sm-9 col-lg-10">
         			<div class="visible-xs-block featured-image-wrapper">
         				</div>
-        			<p>Today's heating and cooling equipment offers advanced technology and systems to dramatically improve energy efficiency and the comfort level of your home. Please click on the equipment's image below to view how we can use these latest systems to meet your comfort and energy efficiency needs.</p>
+        			<h2>Page in Maintenance</h2>
         		</div>
         	</div>
         	        	</div>
